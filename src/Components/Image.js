17117c8d3b62d5c0e0225e286e3801a662d77bfa
Maximo Macchi <maximomacchi@users.noreply.github.com>
2018-11-03 17:50:36 -0700

    Buttons added to image

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -11,7 +11,15 @@ export default class Image extends Component {
   render() {
     if (this.state.imageURL != "") {
       return (
+        <div className="imageComponent">
           <img className="image" src={this.state.imageURL} />
+          <button className="imgButton" id="downloadButton">
+            Download
+          </button>
+          <button className="imgButton" id="shareButton">
+            Share
+          </button>
+        </div>
       );
     } else {
       return <div>Loading...</div>;
